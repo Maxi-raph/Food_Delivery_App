6fa1e0448b962dd382663f01685711b6b2c23d96
Raph <onwujekwechibuzor@gmail.com>
2026-02-21 22:39:22 +0100

    added popular categories section

--- app/components/NavbarUi.tsx
+++ app/components/NavbarUi.tsx
@@ -10,7 +10,7 @@ const Nav = () => {
 
     return (<>
         {/* Desktop Navigation */} 
-        <div className='hidden md:flex py-3 px-6  space-x-32 justify-between items-center'>
+        <div className='hidden md:flex py-3 px-6  space-x-20 justify-between items-center'>
             <img src="/Images/Onboarding/Chuks-Kitchen.png" alt="logo" className="w-36 h-8"/>
             <div className='flex flex-1 items-center justify-between'>
                 <NavLink to={'/home'} className={({isActive})=>(
@@ -44,28 +44,29 @@ const Nav = () => {
             : <HiX  size={24} className={`cursor-pointer `} onClick={()=>showNav()}/>
             }
             
-                <div className={`p-6 rounded-lg w-64 shadow-lg border border-gray-400 bg-gray-300/30 backdrop-blur-sm absolute top-15 right-2 transition-translate duration-700 ${!isNavOpen ? 'translate-x-100' : 'translate-x-0'}`}>
+                <div className={`p-6 rounded-lg w-64 shadow-lg border border-gray-400 bg-gray-300/30 backdrop-blur-sm absolute z-10 top-15 right-2 
+                    transition-translate duration-800 ${!isNavOpen ? 'translate-x-100' : 'translate-x-0'}`}>
                     <div className='flex w-full flex-col items-center space-y-6'>
                         <NavLink to={'/home'} className={({isActive})=>(
                             isActive 
                             ? 'text-[#FF7A18] text-sm'
-                            : 'hover:text-[#FF7A18] text-sm'
-                        )}>Home</NavLink>
+                            : 'hover:text-[#FF7A18] text-sm text-white'
+                        )} onClick={()=>showNav()}>Home</NavLink>
                         <NavLink to={'/onboarding/login'} className={({isActive})=>(
                             isActive 
                             ? 'text-[#FF7A18] text-sm'
-                            : 'hover:text-[#FF7A18] text-sm'
-                        )}>Explore</NavLink>
+                            : 'hover:text-[#FF7A18] text-sm text-white'
+                        )} onClick={()=>showNav()}>Explore</NavLink>
                         <NavLink to={'/onboarding/signUp'} className={({isActive})=>(
                             isActive 
                             ? 'text-[#FF7A18] text-sm'
-                            : 'hover:text-[#FF7A18] text-sm'
-                        )}>My Orders</NavLink>
+                            : 'hover:text-[#FF7A18] text-sm text-white'
+                        )} onClick={()=>showNav()}>My Orders</NavLink>
                         <NavLink to={'/onboarding'} className={({isActive})=>(
                             isActive 
                             ? 'text-[#FF7A18] text-sm'
-                            : 'hover:text-[#FF7A18] text-sm'
-                        )}>Account</NavLink>
+                            : 'hover:text-[#FF7A18] text-sm text-white'
+                        )} onClick={()=>showNav()}>Account</NavLink>
                         <Link to={'/onboarding/login'} className='text-white py-1 px-6 rounded-md
                          bg-[#FF7A18] hover:bg-[#ff6702] text-center text-sm w-full'>
                         Login</Link>
--- app/routes/Home/index.tsx
+++ app/routes/Home/index.tsx
@@ -1,4 +1,7 @@
+import { HiSearch } from "react-icons/hi";
 import type { Route } from "./+types";
+import { Link } from 'react-router'
+import CategoriesCard from "~/components/CategoriesCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,13 +11,64 @@ export function meta({}: Route.MetaArgs) {
 }
 
 const HomePage = () => {
-    
+    const popularCategories = [
+      {img:'/Images/Onboarding/Image-8.png',
+       name:'Jollof Delights',
+       id:1
+      },
+      {img:'/Images/Onboarding/Image-2.png',
+       name:'Swallow & Soups',
+       id:2
+      },
+      {img:'/Images/Onboarding/Image-3.png',
+       name:'Grills & BBQ',
+       id:3
+      },
+      {img:'/Images/Onboarding/Image-4.png',
+       name:'Sweet Treats',
+       id:4
+      },
+      {img:'/Images/Onboarding/Image-5.png',
+       name:'Jollof Delights',
+       id:5
+      },
+      {img:'/Images/Onboarding/Image-6.png',
+       name:'Jollof Delights',
+       id:6
+      }
+    ]
     return ( 
         <>
-                <div className="min-h-[47.4vh]  bg-gray-100">
-                    <h1>Welcome to Chuks Kitchen!</h1>
-                    <p>Your content here</p>
-                </div>
+          <section 
+            className="bg-cover bg-center bg-no-repeat py-16 px-8 min-h-screen relative"
+            style={{ 
+            backgroundImage: `url('/Images/Onboarding/bg-1.jpeg')`,
+            backgroundColor: 'rgba(0,0,0,0.8)',
+            backgroundBlendMode: 'overlay'
+          }}>
+            <h1 className="text-5xl font-bold text-white pt-36 mb-6">
+              The Heart of Nigerian Home Cooking
+            </h1>
+            <p className="text-white mb-8 text-2xl">Handcrafted with passion, delivered with care.</p>
+            <Link to={'/home'} className="text-white py-3 px-5
+             text-center bg-[#FF7A18] rounded-md">
+            Discover what's new</Link>
+            <div className="absolute -bottom-4 flex justify-center w-full">
+              <div className="relative w-full flex justify-center">
+                <HiSearch className="absolute top-3 left-[19.8%] text-xl" color="gray"/>
+                <input type="text" placeholder="What are you craving for today?"
+                className="w-[62%] pl-8 pr-3 py-2 rounded-md shadow-md bg-white focus:ring-gray-300 focus:ring-2 outline-0"/>
+              </div>
+            </div>
+          </section>
+          <section className="bg-gray-200 py-16 px-12">
+            <h2 className="text-xl text-center mb-6 font-bold">Popular Categories</h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
+              {popularCategories.map(item =>(
+                  <CategoriesCard item={item}/>
+              ))}
+            </div>
+          </section>
         </>
     );
 }
